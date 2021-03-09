2584630e3c7c8414c7dee8a07c07953dda6de408
kirin <kirin.chen1001@gmail.com>
2021-03-09 21:11:08 +0800

    done the CoinFetcher

--- Coin.py
+++ Coin.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup, Tag
 from datetime import datetime
 from enum import Enum
+import time
 
 HOST = 'https://coinmarketcap.com/'
 COINS_PAGE_URL: str = HOST + 'coins/?page=%i'
@@ -9,9 +10,11 @@ Historical_SUBFIX = 'historical-data/'
 
 
 class CoinInfo:
-    def __init__(self, n: str, s: str):
+
+    def __init__(self, n: str, s: str, i: int):
         self.name = n
         self.symbol = s
+        self.id = i
         self.path = 'currencies/' + self.name + '/'
 
     def getHistoricalPath(self):
@@ -26,18 +29,25 @@ class CoinInfo:
         ans = temp.format(s.strftime(fomt), e.strftime(fomt))
         return ans
 
+    def getOhlcvHistoricalUrl(self, s: datetime, e: datetime):
+        sts = int( s.timestamp())
+        ets = int(e.timestamp())
+        temp = 'https://web-api.coinmarketcap.com/v1/cryptocurrency/ohlcv/historical?id={0}&convert=USD&time_end={1}&time_start={2}'
+        return temp.format(self.id, ets,sts)
+
 
 class BuiltInCoin(Enum):
 
-    def __init__(self, n: str, s: str):
+    def __init__(self, n: str, s: str, i: int):
         self.pid = n
+        self.id = i
         self.symbol = s
 
-    BTC = ('bitcoin', 'BTC')
-    ETH = ('ethereum', 'ETH')
+    BTC = ('bitcoin', 'BTC', 1)
+    ETH = ('ethereum', 'ETH', 3)
 
     def getCoinInfo(self):
-        return CoinInfo(self.pid, self.symbol)
+        return CoinInfo(self.pid, self.symbol, self.id)
 
 
 def loadSymbols():
@@ -107,9 +117,9 @@ if __name__ == '__main__':
             self.assertIsNotNone(result)
 
         def test_getCoinPageUrl(self):
-            c = CoinInfo('BTC', 'BTC')
-            c.path = 'test'
-            u = c.getDateHistoricalUrl(datetime.now(), datetime.now())
+            c = BuiltInCoin.BTC
+            st = datetime(2021,3,3)
+            u = c.getCoinInfo().getOhlcvHistoricalUrl(st, datetime.now())
             print(u)
             self.assertIsNotNone(u)
 
--- CoinFetcher.py
+++ CoinFetcher.py
@@ -1,6 +1,8 @@
 from Coin import CoinInfo, BuiltInCoin
 from datetime import datetime, date
 from CMCBrowserUtils import getBrowser, WebDriverWait, EC, By
+import requests, json
+
 
 # https://web-api.coinmarketcap.com/v1/cryptocurrency/ohlcv/historical?id=1&convert=USD&time_start=1201245530&time_end=1615218330
 
@@ -15,21 +17,27 @@ class CoinFetcher:
         bser.get(self.info.getDateHistoricalUrl(sd, datetime.now()))
         WebDriverWait(bser, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".reply-button"))).click()
 
+    def parseOhlcvHistorical(self, s: datetime, e: datetime):
+        url = self.info.getOhlcvHistoricalUrl(s, e)
+        resp = requests.get(url)
+        jr = json.loads(resp.text)
+        return jr['data']['quotes']
+
 
 if __name__ == '__main__':
     import unittest
 
 
     class SymbolTest(unittest.TestCase):
-        def test_goToHistoricalPage(self):
+        def test_parseOhlcvHistorical(self):
             cf = CoinFetcher(BuiltInCoin.BTC.getCoinInfo())
-            cf.goToHistoricalPage()
-            print(cf)
-            self.assertIsNotNone(cf)
+            ans = cf.parseOhlcvHistorical(datetime(2021, 3, 3), datetime.now())
+            print(ans)
+            self.assertIsNotNone(ans)
 
 
     tests = [
-        SymbolTest('test_goToHistoricalPage'),
+        SymbolTest('test_parseOhlcvHistorical'),
 
     ]
     suite = unittest.TestSuite()
